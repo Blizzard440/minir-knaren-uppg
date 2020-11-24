51ce0309a553f3a8014e200d7e8546e9e7dcf807
Arvid Olsson <70146091+Blizzard440@users.noreply.github.com>
2020-11-24 08:53:04 +0100

    ska kompletera detta när jag får tid

---
+++ Miniräknaren.py
@@ -0,0 +1,21 @@
+Välkommen = str(input('Välkommen till miniräknaren (Enter för att gå vidare) '))
+print('välj räknesätt')
+print('(1) för +')
+print('(2) för -')
+print('(3) för *')
+print('(4) för att avsluta')
+
+
+
+
+
+
+while true:
+    val = int(input('Vilken funktion vill du använda? '))
+    if val in (1, 2, 3, 4):
+        atal1 = float(input('Tal 1:'))
+        atal2 = float(input('Adderat med: '))
+        print (f'Det blir ')
+
+
+
