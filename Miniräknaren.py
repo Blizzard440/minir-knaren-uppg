6902ddb29f12e1e77942cd62856ac72afc8ce1cc
Arvid Olsson <70146091+Blizzard440@users.noreply.github.com>
2020-11-30 13:29:45 +0100

    Update Miniräknaren.py

--- Miniräknaren.py
+++ Miniräknaren.py
@@ -8,4 +8,26 @@ while main != 4:
     
     main = int(input('Välj räknesätt: '))
 
- 
+    if main == 1:
+        print('Räkna med addtition')
+        tal1 = float(input('Tal 1: '))
+        tal2 = float(input('Tal 2: '))
+        summa = tal1 + tal2
+        print(f'summan av {tal1} och {tal2} är {summa} ')
+
+    elif main == 2:
+        print('Räkna med subtraktion')
+        tal1 = float(input('Tal1: '))
+        tal2 = float(input('Tal2: '))
+        differens = tal1 - tal2
+        print(f'differensen mellan {tal1} och{tal2} är {differens}')
+
+    elif main == 3:
+        print('Räkna med multiplikation')
+        tal1 = float(input('Tal1: '))
+        tal2 = float(input('Tal2: '))
+        summa = tal1 * tal2
+        print(f'Summan av {tal2} * {tal2} är {summa}')
+    
+    else:
+        print ('Trevligt att stå till tjänst')
