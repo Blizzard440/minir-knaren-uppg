154bac187d6b2759ec398ece22f37b12c956aa82
Arvid Olsson <70146091+Blizzard440@users.noreply.github.com>
2020-11-30 13:29:06 +0100

    Update Miniräknaren.py

--- Miniräknaren.py
+++ Miniräknaren.py
@@ -1,21 +1,11 @@
-Välkommen = str(input('Välkommen till miniräknaren (Enter för att gå vidare) '))
-print('välj räknesätt')
-print('(1) för +')
-print('(2) för -')
-print('(3) för *')
-print('(4) för att avsluta')
+main = 0
 
+while main != 4:
+    print('(1) för +')
+    print('(2) för -')
+    print('(3) för *')
+    print('(4) för att avsluta')
+    
+    main = int(input('Välj räknesätt: '))
 
-
-
-
-
-while true:
-    val = int(input('Vilken funktion vill du använda? '))
-    if val in (1, 2, 3, 4):
-        atal1 = float(input('Tal 1:'))
-        atal2 = float(input('Adderat med: '))
-        print (f'Det blir ')
-
-
-
+ 
